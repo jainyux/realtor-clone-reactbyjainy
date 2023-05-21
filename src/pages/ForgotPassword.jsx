b618b8432410071226954bbe0c1cb10f27ba4d8e
jainyux <125820666+jainyux@users.noreply.github.com>
2023-05-21 09:06:27 +0100

    complete the forgot password functinality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 
 import {Link} from 'react-router-dom'
 import QAuth from '../components/OAuth';
+import { toast } from 'react-toastify';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 
 export default function ForgotPassword() {
  
@@ -12,6 +14,17 @@ setEmail(e.target.value);
   
 
   }
+  async function onSubmit(e){
+e.preventDefault()
+try {
+  const auth = getAuth()
+  await sendPasswordResetEmail(auth, email)
+  toast.success("Email was sent")
+} catch (error) {
+  toast.error("Could not send reset password")
+  
+}
+  }
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Forgot Password</h1>
@@ -20,7 +33,7 @@ setEmail(e.target.value);
           <img src='https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8a2V5fGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60' alt='key' className='w-full rounded-2xl'/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form >
+          <form onSubmit={onSubmit}>
             <input  type='email' id="email" value={email} onChange={onChange} placeholder='Email address' className=" mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" />
             
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
